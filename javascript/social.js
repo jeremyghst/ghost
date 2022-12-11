12fc923c8b3866bc31e5cc13967ebdefe17bc18f
jeremyghst <51990006+jeremyghst@users.noreply.github.com>
2022-12-11 21:19:28 +0100

    purple ghost

--- javascript/social.js
+++ javascript/social.js
@@ -2,7 +2,6 @@
 
 function fadeSocial(dir){
     const socials = Array.from(document.getElementsByClassName('socialFadein'));
-    const socialmedia = document.getElementById('socialmedia')
 
     socials.forEach(social => {
 
@@ -10,21 +9,9 @@ function fadeSocial(dir){
             if(!social.classList.contains('visible')){
                 social.classList.add('visible');
             }
-
-            if(!socialmedia.classList.contains('visible')){
-                socialmedia.classList.add('visible');
-
-                setTimeout(() => {
-                    social.scrollIntoView({behavior: "smooth", block: "start"});
-                }, 1750)
-            }
         } else {
             if(social.classList.contains('visible')){
                 social.classList.remove('visible');
-            }
-
-            if(socialmedia.classList.contains('visible')){
-                socialmedia.classList.remove('visible');
             }
         }
     })
