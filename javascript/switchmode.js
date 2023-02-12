f06c4255865b847a3316f0119b5067d8dfe3c661
jeremyghst <51990006+jeremyghst@users.noreply.github.com>
2023-02-12 18:33:16 +0100

    fox mode

--- javascript/switchmode.js
+++ javascript/switchmode.js
@@ -7,7 +7,11 @@ switchBtn.addEventListener('click', switchFunc);
 
 const bodyElement = document.getElementById('body');
 const heading_logo = document.getElementById('heading_logo');
+
+const gh_logo = document.getElementById('gh');
 const hero_logo = document.getElementById('boo');
+const st_logo = document.getElementById('st');
+
 const balance_logo = document.getElementById('balance');
 const footer_logo = document.getElementById('tag');
 const favicon_logo = document.getElementById('favicon');
@@ -20,7 +24,9 @@ function switchFunc(){
     if(state === 'ghost'){
         bodyElement.classList.add('foxMode');
         heading_logo.src = './assets/fox.svg';
+        gh_logo.src = './assets/gh_fox.svg';
         hero_logo.src = './assets/fox_bucket.svg';
+        st_logo.src = './assets/st_fox.svg';
         balance_logo.src = './assets/balance_fox.svg';
         footer_logo.src = './assets/tag_fox.png';
         favicon_logo.href = './assets/fox.svg';
@@ -33,7 +39,9 @@ function switchFunc(){
     } else {
         bodyElement.classList.remove('foxMode');
         heading_logo.src = './assets/ghost.svg';
+        gh_logo.src = './assets/gh_ghost.svg';
         hero_logo.src = './assets/ghost_beanie.svg';
+        st_logo.src = './assets/st_ghost.svg';
         balance_logo.src = './assets/balance_ghost.svg';
         footer_logo.src = './assets/tag_ghost.png';
         favicon_logo.href = './assets/ghost.svg';
