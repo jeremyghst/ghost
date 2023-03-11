4caf53569708b123aae17b223fc78ebc01501c7e
jeremyghst <51990006+jeremyghst@users.noreply.github.com>
2023-03-12 00:09:59 +0100

    slogan 2

--- javascript/update/updateMode.js
+++ javascript/update/updateMode.js
@@ -42,7 +42,7 @@ const modeObj = [
         greeting: 'Boooooo!',
         slogan: 'balance is key',
         information: component_me,
-        slogan2: 'share your thoughts'
+        slogan2: 'heart versus mind'
     },
     {
         id: 'fox',
